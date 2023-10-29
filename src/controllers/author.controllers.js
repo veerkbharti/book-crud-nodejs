0ff55d1a96736da5f0dc3f7b7c9b7f2980284442
Veer Kunwer Bharti <veer7549kr@gmail.com>
2023-10-29 20:34:28 +0530

    Implemented get all authors and get single author operation

--- src/controllers/author.controllers.js
+++ src/controllers/author.controllers.js
@@ -1,11 +1,26 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Author from "../models/author.model.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 export const addAuthor = catchAsyncErrors(async (req, res, next) => {
-    console.log(req.body);
   const author = await Author.create(req.body);
 
   res
     .status(200)
     .json({ success: true, message: "Author added successfully", author });
+});
+
+export const getAllAuthors = catchAsyncErrors(async (req, res, next) => {
+  const authors = await Author.find();
+
+  res.status(200).json({ success: true, authors });
+});
+
+export const getSingleAuthor = catchAsyncErrors(async (req, res, next) => {
+  const author = await Author.findById(req.params.id);
+
+  if (!author)
+    return next(new ErrorHandler("Author not found", 404))
+
+    res.status(200).json({ success: true, author });
 });
--- src/routes/author.routes.js
+++ src/routes/author.routes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { addAuthor } from "../controllers/author.controllers.js";
+import {
+  addAuthor,
+  getAllAuthors,
+  getSingleAuthor,
+} from "../controllers/author.controllers.js";
 
 const router = Router();
 
 router.route("/authors/new").post(addAuthor);
+router.route("/authors").get(getAllAuthors);
+router.route("/authors/:id").get(getSingleAuthor);
 
 export default router;
