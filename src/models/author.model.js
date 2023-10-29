5afb5f1099f0949dd9bcd8f8c1cc8a52cb7e8c36
Veer Kunwer Bharti <veer7549kr@gmail.com>
2023-10-29 17:45:44 +0530

    Added Author model

---
+++ src/models/author.model.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const authorSchema = Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+    },
+
+    dateOfBirth: {
+      type: Date,
+    },
+
+    dateOfDeath: {
+      type: Date,
+    },
+
+    nationality: String,
+    biography: String,
+  },
+  { timestamps: true }
+);
+
+const Author = model("Author", authorSchema);
+export default Author;
