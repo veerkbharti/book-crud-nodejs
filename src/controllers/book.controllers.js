2868ce11e6da3da969e1dd38628abe0f1fab3bd2
Veer Kunwer Bharti <veer7549kr@gmail.com>
2023-10-29 21:47:42 +0530

    Implemented update and delete operation for books

--- src/controllers/book.controllers.js
+++ src/controllers/book.controllers.js
@@ -1,5 +1,6 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Book from "../models/book.model.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 export const addBook = catchAsyncErrors(async (req, res, next) => {
   const book = await Book.create(req.body);
@@ -21,4 +22,34 @@ export const getBookDetails = catchAsyncErrors(async (req, res, next) => {
   if (!book) return next(new ErrorHandler("Book not found", 404));
 
   res.status(200).json({ success: true, book });
+});
+
+export const updateBook = catchAsyncErrors(async (req, res, next) => {
+  const bookId = req.params.id;
+  let book = await Book.findById(bookId);
+
+  if (!book) return next(new ErrorHandler("Book not found", 404));
+
+  book = await Book.findByIdAndUpdate(bookId, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res
+    .status(200)
+    .json({ success: true, message: "Book updated succesfully", book });
+});
+
+export const deleteBook = catchAsyncErrors(async (req, res, next) => {
+  const bookId = req.params.id;
+  let book = await Book.findById(bookId);
+
+  if (!book) return next(new ErrorHandler("Book not found", 404));
+
+  book = await Book.findByIdAndDelete(bookId);
+
+  res
+    .status(200)
+    .json({ success: true, message: "Book deleted successfully", book });
 });
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -3,12 +3,18 @@ import {
   addBook,
   getAllBooks,
   getBookDetails,
+  updateBook,
+  deleteBook,
 } from "../controllers/book.controllers.js";
 
 const router = Router();
 
 router.route("/books/new").post(addBook);
 router.route("/books").get(getAllBooks);
-router.route("/books/:id").get(getBookDetails);
+router
+  .route("/books/:id")
+  .get(getBookDetails)
+  .put(updateBook)
+  .delete(deleteBook);
 
 export default router;
